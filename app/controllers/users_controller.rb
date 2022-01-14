fa7718889a3edf2cb2b22474155e34cc3c30dc72
Miyawaki <tetsu.miyawaki@gmail.com>
2022-01-14 01:43:56 +0000

    [Finish]4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = ユーザーの取得
-    @user.ユーザーのアップデート
-    redirect_to ユーザーの詳細ページへのパス
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to edit_user_path(@user.id)
   end
 
   private
